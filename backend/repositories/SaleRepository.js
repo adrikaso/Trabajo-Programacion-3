d01e844c642fb67e4097aca158751126978a9b41
Adrian <adr1337@outlook.com>
2025-06-10 20:25:22 -0300

    Baja de la carpeta repositories

--- backend/repositories/SaleRepository.js
+++
@@ -1,9 +0,0 @@
-const Sale = require('../models/SaleModel');
-
-const getAll = () => Sale.find();
-const getById = id => Sale.findById(id);
-const create = data => Sale.create(data);
-const update = (id, data) => Sale.findByIdAndUpdate(id, data, { new: true });
-const remove = id => Sale.findByIdAndDelete(id);
-
-module.exports = { getAll, getById, create, update, remove };
