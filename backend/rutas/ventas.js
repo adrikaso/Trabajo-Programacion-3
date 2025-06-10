f0d1082f19b99eb7d209c20ba2b9e71fccddd7f5
Adrian <adr1337@outlook.com>
2025-06-10 20:11:08 -0300

    Baja de carpeta rutas

--- backend/rutas/ventas.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const ventaController = require("../controllers/saleController");
-
-router.post("/", ventaController.crearVenta);
-
-module.exports = router;
