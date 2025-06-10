7bde1e9c50a0c9d8ae32f0e6dbf9013968725efb
Adrian <adr1337@outlook.com>
2025-06-10 20:24:17 -0300

    Baja de la carpeta models

--- backend/models/SaleModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const ventaSchema = new mongoose.Schema({
-  clienteNombre: { type: String, default: "" },
-  fecha: { type: Date, default: Date.now },
-  total: { type: Number, required: true }
-});
-
-module.exports = mongoose.model("Venta", ventaSchema);
