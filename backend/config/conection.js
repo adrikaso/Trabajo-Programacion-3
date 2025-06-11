afb164a962ec69078dee1da5452c2758282eb409
Adrian <adr1337@outlook.com>
2025-06-11 19:39:15 -0300

    Eliminación de atributos deprecados

--- backend/config/conection.js
+++ backend/config/conection.js
@@ -2,14 +2,11 @@ const mongoose = require("mongoose");
 
 const conectWithDataBase = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("Conexión a MongoDB exitosa");
   } catch (err) {
     console.error("Error al conectar a MongoDB:", err.message);
-    process.exit(1); 
+    process.exit(1);
   }
 };
 
