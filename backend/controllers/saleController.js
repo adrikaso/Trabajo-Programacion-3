f72755b834861f5226e71133bdd25f33cca96316
Adrian <adr1337@outlook.com>
2025-06-10 21:04:36 -0300

    Corrección de errores

--- backend/controllers/saleController.js
+++ backend/controllers/saleController.js
@@ -1,13 +1,9 @@
-const Venta = require("../models/SaleModel");
-const DetalleVenta = require("../models/SaleDetailsModel");
-
-
 const services = require("../services/SaleService");
 
 const create = async (req, res) => {
   try {
-    const product = await services.createProduct(req.body);
-    res.status(201).json(product);
+    const sale = await services.createSale(req.body);
+    res.status(201).json(sale);
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
@@ -15,75 +11,38 @@ const create = async (req, res) => {
 
 const findAll = async (req, res) => {
   try {
-    const list = await services.getAllProducts();
+    const list = await services.getAllSales();
     res.status(201).json(list);
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
 }
 
-const findProduct = async (req, res) => {
+const findSale = async (req, res) => {
   try {
-    const product = await services.getProductById(req.body);
-    res.status(201).json(product);
+    const sale = await services.getSaleById(req.body);
+    res.status(201).json(sale);
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
 }
 
-const deleteProduct = async (req, res) => {
+const deleteSale = async (req, res) => {
   try {
-    const product = await services.deleteById(req.body);
-    res.status(201).json(product);
+    const sale = await services.deleteSaleById(req.body);
+    res.status(201).json(sale);
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
 }
 
-const updateProduct = async (req, res) => {
+const updateSale = async (req, res) => {
   try {
-    const product = await services.updateById(req.body);
-    res.status(201).json(product);
+    const sale = await services.updateSaleById(req.body);
+    res.status(201).json(sale);
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
 }
 
-module.exports = { create, deleteProduct, findAll, findProduct, deleteProduct, updateProduct };
-
-
-
-
-
-
-
-
-
-
-
-
-
-// exports.crearVenta = async (req, res) => {
-//   try {
-//     const { clienteNombre, carrito } = req.body;
-
-//     const total = carrito.reduce((sum, item) => sum + item.precio * item.cantidad, 0);
-
-//     const venta = new Venta({ clienteNombre, total });
-//     const ventaGuardada = await venta.save();
-
-//     const detalles = carrito.map(item => ({
-//       ventaId: ventaGuardada._id,
-//       productoId: item._id,
-//       cantidad: item.cantidad,
-//       subtotal: item.precio * item.cantidad
-//     }));
-
-//     await DetalleVenta.insertMany(detalles);
-
-//     res.status(201).json({ mensaje: "Venta registrada con éxito", ventaId: ventaGuardada._id });
-//   } catch (error) {
-//     console.error("Error al crear venta:", error);
-//     res.status(500).json({ mensaje: "Error al registrar la venta", error });
-//   }
-// };
+module.exports = { create, findAll, findSale, deleteSale, updateSale };
