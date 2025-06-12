926a6c873abe96364d7b2352fa790fa1f453c32b
Adrian <adr1337@outlook.com>
2025-06-12 19:12:13 -0300

    Sin modificaciones

--- backend/persistence/models/ProductModel.js
+++ backend/persistence/models/ProductModel.js
@@ -8,12 +8,6 @@ const productoSchema = new mongoose.Schema({
   activo: { type: Boolean, default: true }
 });
 
-productoSchema.set("toJSON", {
-  transform: function (doc, ret) {
-    ret.id = ret._id;
-    delete ret._id;
-    delete ret.__v;
-  }
-});
+
 
 module.exports = mongoose.model("Producto", productoSchema);
