d2921525021bd380b0d3526f7e1b73558f383ba9
Adrian <adr1337@outlook.com>
2025-06-12 21:40:08 -0300

    Corrección de envío de ID a los metodos del service

--- backend/controllers/SaleDetailsController.js
+++ backend/controllers/SaleDetailsController.js
@@ -20,7 +20,8 @@ const findAll = async (req, res) => {
 
 const findSaleDetails = async (req, res) => {
     try {
-        const saleDetails = await services.getSaleDetailsById(req.body);
+        const { id } = req.params;
+        const saleDetails = await services.getSaleDetailsById(id);
         res.status(201).json(saleDetails);
     } catch (error) {
         res.status(400).json({ error: error.message });
@@ -29,7 +30,8 @@ const findSaleDetails = async (req, res) => {
 
 const deleteSaleDetails = async (req, res) => {
     try {
-        const saleDetails = await services.deleteById(req.body);
+        const { id } = req.params;
+        const saleDetails = await services.deleteById(id);
         res.status(201).json(saleDetails);
     } catch (error) {
         res.status(400).json({ error: error.message });
@@ -38,7 +40,8 @@ const deleteSaleDetails = async (req, res) => {
 
 const updateSaleDetails = async (req, res) => {
     try {
-        const saleDetails = await services.updateById(req.body);
+        const { id } = req.params;
+        const saleDetails = await services.updateById(id, req.body);
         res.status(201).json(saleDetails);
     } catch (error) {
         res.status(400).json({ error: error.message });
