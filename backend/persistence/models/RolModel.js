7f5ee351841ad2a69cee6a3d844e8bcff33fa9e6
Daniel-iwach <daniel.g.iwach@gmail.com>
2025-06-16 11:03:38 -0300

    Creacion del schema rol

---
+++ backend/persistence/models/RolModel.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose");
+
+const rolSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+});
+
+module.exports = mongoose.model("Roles", rolSchema);
