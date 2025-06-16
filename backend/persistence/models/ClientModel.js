e0d65d74ceaa352ef9424fe87abf7e4566dffdc9
Daniel-iwach <daniel.g.iwach@gmail.com>
2025-06-16 11:03:17 -0300

    Creacion del schema cliente

---
+++ backend/persistence/models/ClientModel.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose");
+
+const clientSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+});
+
+module.exports = mongoose.model("Client", clientSchema);
