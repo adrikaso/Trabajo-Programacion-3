772df51c7ce824bef942eb6fc42177daaf733225
Daniel-iwach <daniel.g.iwach@gmail.com>
2025-06-16 11:16:24 -0300

    Actualizacion del schema de espanol a ingles

--- backend/persistence/models/ProductModel.js
+++ backend/persistence/models/ProductModel.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
 
-const productoSchema = new mongoose.Schema({
-  nombre: { type: String, required: true },
-  precio: { type: Number, required: true },
-  imagen: { type: String, required: true },
-  categoria: { type: String, enum: ["suplemento", "accesorio"], required: true },
-  activo: { type: Boolean, default: true }
+const productSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  price: { type: Number, required: true },
+  pictureurURL: { type: String, required: true },
+  categorY: { type: String, enum: ["suplemento", "accesorio"], required: true },
+  active: { type: Boolean, default: true }
 });
 
 
 
-module.exports = mongoose.model("Producto", productoSchema);
+module.exports = mongoose.model("Product", productSchema);
