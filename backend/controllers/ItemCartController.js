8c5e42d2df2f1106e552f3d079343497955bf3c6
Adrian <adr1337@outlook.com>
2025-06-16 23:01:02 -0300

    Creación de ItemCartController

---
+++ backend/controllers/ItemCartController.js
@@ -0,0 +1,28 @@
+const services = require("../services/ItemCartService");
+
+const getAllItemCarts = async (req, res) => {
+    const itemCarts = await services.getAllItemCarts();
+    res.json(itemCarts);
+};
+
+//EN CREAR ME TIENE QUE LLEGAR EL ID DEL PRODUCTO, EL PRODUCTNAME, PRODUCT PRICE Y QUANTITY
+const createItemCart = async (req, res) => {
+    const { productId ,productName, unitPrice, quantity } = req.body;
+
+    const itemCart = await services.createItemCart({ productId, productName, unitPrice, quantity });
+    res.json(itemCart);
+};
+
+const updateItemCart = async (req, res) => {
+    const { id } = req.params;
+    const itemCart = await services.updateItemCart(id, req.body);
+    res.json(itemCart);
+};
+
+const deleteItemCart = async (req, res) => {
+    const { id } = req.params;
+    const itemCart = await services.deleteItemCart(id);
+    res.json(itemCart);
+};
+
+module.exports = { getAllItemCarts, createItemCart, updateItemCart, deleteItemCart };
