6c1651c572dd04b8d337782f2833cf2f8e4f6808
Daniel-iwach <daniel.g.iwach@gmail.com>
2025-06-17 19:02:58 -0300

    implementacion de metodos para borrar todo

--- backend/persistence/models/SaleDetailsModel.js
+++ backend/persistence/models/SaleDetailsModel.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 
 const saleDetailsSchema = new mongoose.Schema({
-  saleId: { type: mongoose.Schema.Types.ObjectId, ref: "Venta", required: true },
-  productId: { type: mongoose.Schema.Types.ObjectId, ref: "Producto", required: true },
+  saleId: { type:String, required: true },
+  productId: { type: String, required: true },
   quantity: { type: Number, required: true },
   subtotal: { type: Number, required: true }
 });
