1bd3a7de3ec081ee44ceb252b5f1577f327ef52b
Daniel-iwach <daniel.g.iwach@gmail.com>
2025-06-17 19:02:33 -0300

    implementacion de metodos para borrar todo

--- backend/persistence/models/ItemCartModel.js
+++ backend/persistence/models/ItemCartModel.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 
 const itemCartSchema = new mongoose.Schema({
-    shopingCartId: { type: mongoose.Schema.Types.ObjectId, ref: "ShopingCart", required: true },
-    productId: { type: mongoose.Schema.Types.ObjectId, ref: "Product", required: true },
+    shopingCartId: { type: String, required: true },
+    productId: { type: String, required: true },
     productName: { type: String, required: true },
     unitPrice: { type: Number, required: true },
     quantity: { type: Number, required: true }
