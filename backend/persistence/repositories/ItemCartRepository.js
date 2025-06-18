bb2f88c5b754cb1b8004fe6a981b1456a0be3f0a
Adrian <adr1337@outlook.com>
2025-06-17 22:37:48 -0300

    Agregar funcion updateQuantity en ItemCartRepository

--- backend/persistence/repositories/ItemCartRepository.js
+++ backend/persistence/repositories/ItemCartRepository.js
@@ -4,7 +4,8 @@ const getAll = () => ItemCart.find();
 const getById = id => ItemCart.findById(id);
 const create = data => ItemCart.create(data);
 const update = (id, data) => ItemCart.findByIdAndUpdate(id, data, { new: true });
+const updateQuantity = (id, data) => ItemCart.updateOne({ _id: id}, data);
 const remove = id => ItemCart.findByIdAndDelete(id);
 const removeAll = () => ItemCart.deleteMany();
 
-module.exports = { getAll, getById, create, update, remove, removeAll };
+module.exports = { getAll, getById, create, update, updateQuantity, remove, removeAll };
