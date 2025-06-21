172f8d10b91fb62e9d4a8ebd1a124bea6e9e8eba
Daniel-iwach <daniel.g.iwach@gmail.com>
2025-06-21 11:06:20 -0300

    implementa ruta para acceder al metodo de obtener el total del carrito

--- backend/routes/ShopingCartRoutes.js
+++ backend/routes/ShopingCartRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const shopingCartController = require('../controllers/ShopingCartController');
 
 router.get('/getAll', shopingCartController.getAllShopingCarts);
+router.get('/getTotal', shopingCartController.getTotal);
 router.post('/create', shopingCartController.createShopingCart);
 router.delete('/delete', shopingCartController.deleteShopingCart);
 module.exports = router;
