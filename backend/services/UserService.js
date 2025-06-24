7a910546bb396d6c5683d883b59169b2c061ea10
Daniel-iwach <daniel.g.iwach@gmail.com>
2025-06-24 14:30:54 -0300

    actualizacion de logica

--- backend/services/UserService.js
+++ backend/services/UserService.js
@@ -1,6 +1,10 @@
+
 const repo = require('../persistence/repositories/UserRepository');
+const bcrypt = require("bcryptjs");
 
 async function createUser(data) {
+    const salt = await bcrypt.genSalt(10); 
+    data.password = await bcrypt.hash(data.password, salt);
     return await repo.create(data);
 }
 
@@ -12,6 +16,10 @@ async function getUserById(id) {
     return await repo.getById(id);
 }
 
+async function getUserByEmail(email) {
+    return await repo.getByEmail(email);
+}
+
 async function updateUserById(id, userUpdated) {
     return await repo.update(id, userUpdated);
 }
@@ -20,4 +28,4 @@ async function deleteUserById(id) {
     return await repo.remove(id);
 }
 
-module.exports = { createUser, getAllUsers, getUserById, updateUserById, deleteUserById };
+module.exports = { createUser, getAllUsers,getUserByEmail, updateUserById, getUserById, deleteUserById };
