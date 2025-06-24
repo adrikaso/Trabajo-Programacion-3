12fffaf123b8b59fe754d2a0cd994ccc3ad6cd55
Daniel-iwach <daniel.g.iwach@gmail.com>
2025-06-24 14:27:59 -0300

    actualizacion de modelado

--- backend/persistence/models/UserModel.js
+++ backend/persistence/models/UserModel.js
@@ -5,7 +5,9 @@ const userScheme = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     date: { type: Date, default: Date.now },
-    rol: { type: mongoose.Schema.Types.ObjectId, ref: "Roles", required: true }
+    rol: { type: mongoose.Schema.Types.ObjectId, ref: "Roles", required: true },
+    products: [{ type: mongoose.Schema.Types.ObjectId, ref: "Product" }],
+    sales: [{ type: mongoose.Schema.Types.ObjectId, ref: "Sale" }]
 });
 
 module.exports = mongoose.model("User", userScheme);
