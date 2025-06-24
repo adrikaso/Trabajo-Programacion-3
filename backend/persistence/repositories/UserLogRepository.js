ade855fa3690370411283840e6febb5859ce26b9
Daniel-iwach <daniel.g.iwach@gmail.com>
2025-06-24 14:29:11 -0300

    implementacion de nuevos metodos necesarios

--- backend/persistence/repositories/UserLogRepository.js
+++ backend/persistence/repositories/UserLogRepository.js
@@ -1,9 +1,9 @@
-const SaleDetails = require('../models/UserLogModel');
+const userLog = require('../models/UserLogModel');
 
-const getAll = () => SaleDetails.find();
-const getById = id => SaleDetails.findById(id);
-const create = data => SaleDetails.create(data);
-const update = (id, data) => SaleDetails.findByIdAndUpdate(id, data, { new: true });
-const remove = id => SaleDetails.findByIdAndDelete(id);
+const getAll = () => userLog.find();
+const getById = id => userLog.findById(id);
+const create = data => userLog.create(data);
+const update = (id, data) => userLog.findByIdAndUpdate(id, data, { new: true });
+const remove = id => userLog.findByIdAndDelete(id);
 
 module.exports = { getAll, getById, create, update, remove };
