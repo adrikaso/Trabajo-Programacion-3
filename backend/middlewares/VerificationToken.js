f34858ec058497413ac263203be700cfabd49087
Daniel-iwach <daniel.g.iwach@gmail.com>
2025-06-24 21:05:18 -0300

    implementacion de middlewares

--- backend/middlewares/VerificationToken.js
+++ backend/middlewares/VerificationToken.js
@@ -1,14 +1,21 @@
-function verificarToken(req, res, next) {
+const jwt = require("jsonwebtoken");
+
+function verificationToken(req, res, next) {
     const authHeader = req.headers.authorization;   //obtiene el header de la peticion de autenticacion 
     const token = authHeader?.split(" ")[1];    //obtiene el token del header de la peticion
+
+    console.log("Token recibido:", token);
 
     if (!token) return res.status(401).json({ mensaje: "Token no enviado" });
 
     try {
-        const decoded = jwt.verify(token, "clave_secreta");
-        req.usuario = decoded; // Se guarda el payload (con rol incluido)
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        console.log("Token decodificado:", decoded);
+        req.user = decoded; // Se guarda el payload (con rol incluido)
         next();
     } catch (err) {
         return res.status(403).json({ mensaje: "Token inválido" });
     }
 }
+
+module.exports = verificationToken;
