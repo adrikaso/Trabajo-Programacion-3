2aefe11c0b934f42cceff820a869db3a711769c6
Daniel-iwach <daniel.g.iwach@gmail.com>
2025-06-30 14:25:14 -0300

    actualizacion de modelado para poder recibir varios roles

--- backend/persistence/models/UserModel.js
+++ backend/persistence/models/UserModel.js
@@ -5,7 +5,7 @@ const userScheme = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     date: { type: Date, default: Date.now },
-    rol: { type: mongoose.Schema.Types.ObjectId, ref: "Roles", required: true },
+    rol: [{ type: mongoose.Schema.Types.ObjectId, ref: "Roles", required: true }],
     products: [{ type: mongoose.Schema.Types.ObjectId, ref: "Product" }],
     sales: [{ type: mongoose.Schema.Types.ObjectId, ref: "Sale" }]
 });
