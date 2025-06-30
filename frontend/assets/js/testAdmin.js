e64e8b18aff12cb163c6beefde1f0489412f602d
Daniel-iwach <daniel.g.iwach@gmail.com>
2025-06-30 14:35:39 -0300

    implementacion de metodos fetch

--- frontend/assets/js/testAdmin.js
+++ frontend/assets/js/testAdmin.js
@@ -1,25 +1,33 @@
 document.addEventListener('DOMContentLoaded', () => {
-    
+
     const emailInput = document.getElementById('email');
     const passwordInput = document.getElementById('password');
     const form = document.getElementById('loginForm');
     const btnRoles = document.getElementById('btnRoles');
     const btnShowRoles = document.getElementById('btnShowRoles');
-    const btnLogin= document.getElementById('btnLogin');
+    const btnLogin = document.getElementById('btnLogin');
     const btnLogout = document.getElementById('btnLogout');
+
+    const inputName = document.getElementById('name');
+    const inputEmail = document.getElementById('emailCreate');
+    const inputPassword = document.getElementById('passwordCreate');
+
+    const btnCreateUser = document.getElementById('btnCreateUser');
+
+
 
     btnLogin.addEventListener('click', validateCredentials);
     btnRoles.addEventListener('click', createRoles);
     btnShowRoles.addEventListener('click', showRoles);
     btnLogout.addEventListener('click', logout);
+    btnCreateUser.addEventListener('click', newUser);
 
-    let token = localStorage.getItem('token');
 
-    const userRegister={
+    let userRegister = {
         _id: null,
         name: null,
         email: null,
-        password:null,
+        password: null,
         date: null,
         rol: [],
         products: [],
@@ -43,23 +51,42 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    async function newUser() {
+    async function newUser(event) {
+        event.preventDefault();
+        const selectedRoles = Array.from(document.querySelectorAll('input[name="roles"]:checked')).map(checkbox => checkbox.value);
+        if(selectedRoles.length === 0) {
+            alert('Debes seleccionar al menos un rol');
+            return;
+        }
+
+        const rolList = await getRolIds(selectedRoles);
+
         const userData = {
-            name: "admin",
-            email: "admin@example.com",
-            password:"admin123",
+            name: inputName.value,
+            email: inputEmail.value,
+            password: inputPassword.value,
+
             date: new Date().toISOString(),
-            rol: ["685983724fa7cfaedc0cfafa"],
+            rol: rolList,
             products: [],
             sales: []
         };
+        console.log(userData);
         await createUser(userData);
-        return;
+        
     }
 
-    async function createRoles(){
-        const roleList = ["admin", "superAdmin" ,"user"];
-        for(let rol of roleList){
+    async function getRolIds(rolNames) {
+        const roles = await Promise.all(rolNames.map(async (rolName) => {
+            const rol = await findRolByName(rolName);
+            return rol._id;
+        }));
+        return roles;
+    }
+
+    async function createRoles() {
+        const roleList = ["admin", "superAdmin", "user"];
+        for (let rol of roleList) {
             await createRole(rol);
         }
     }
@@ -83,6 +110,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     async function showRoles() {
         try {
+            let token = localStorage.getItem('token');
             const response = await fetch('http://localhost:3000/rol/getAll', {
                 method: 'GET',
                 headers: {
@@ -113,7 +141,7 @@ document.addEventListener('DOMContentLoaded', () => {
             console.error('Error getting user by email:', error);
             throw error;
         }
-        
+
     }
 
     async function authenticateUser(email, password) {
@@ -126,6 +154,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 body: JSON.stringify({ email, password }),
             });
             const data = await response.json();
+
             return data;
         } catch (error) {
             console.error('Error authenticating user:', error);
@@ -136,17 +165,34 @@ document.addEventListener('DOMContentLoaded', () => {
     async function validateCredentials() {
         const email = emailInput.value;
         const password = passwordInput.value;
-        // const credentials=await getByEmail(email);
-        const credentials=await authenticateUser(email,password);
+        let credentials = await authenticateUser(email, password);
+
         console.log(credentials);
-        localStorage.setItem('token',credentials.token);
+        localStorage.setItem('token', credentials.token);
+
+        if (credentials != null) {
+            console.log(credentials);
+            console.log("entor la balubi")
+            let user = await getByEmail(email);
+            userRegister = user;
+        
+            await createUserLog(user._id, "login");
+            window.location.href = 'menuAdmin.html';
+        }
+
     }
 
     async function logout() {
-        await createUserLog(userRegister._id,"logout");
+        if (userRegister.name != null) {
+            localStorage.removeItem('token');
+            await createUserLog(userRegister._id, "logout");
+            userRegister = null;
+            console.log("Logout exitoso");
+            clearForm();
+        }
     }
 
-    async function createUserLog(userId,action){
+    async function createUserLog(userId, action) {
         try {
             const response = await fetch('http://localhost:3000/userLog/create', {
                 method: 'POST',
@@ -161,6 +207,27 @@ document.addEventListener('DOMContentLoaded', () => {
             console.error('Error creating user log:', error);
             throw error;
         }
+    }
 
+    async function findRolByName(rolName) {
+        try {
+            const token = localStorage.getItem('token');
+            const response = await fetch(`http://localhost:3000/rol/getRol/${rolName}`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    "Authorization": `Bearer ${token}`,
+                },
+            });
+            const data = await response.json();
+            return data;
+        } catch (error) {
+            console.error('Error finding rol by name:', error);
+            throw error;
+        }
+    }
+    function clearForm() {
+        emailInput.value = '';
+        passwordInput.value = '';
     }
 });
