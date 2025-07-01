836fdb72060dcacbe669d1151dd3a445fb1a97eb
Adrian <adr1337@outlook.com>
2025-07-01 03:33:03 -0300

    Corrección a la relación entre itemCart y Product

--- backend/persistence/models/ItemCartModel.js
+++ backend/persistence/models/ItemCartModel.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const itemCartSchema = new mongoose.Schema({
     shopingCartId: { type: String, required: true },
-    productId: { type: String, required: true },
+    productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
     productName: { type: String, required: true },
     unitPrice: { type: Number, required: true },
     quantity: { type: Number, required: true }
