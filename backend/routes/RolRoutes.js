4ba5b89ad8df7ad0a53526f8723c25721dabb4f0
Adrian <adr1337@outlook.com>
2025-07-01 11:14:47 -0300

    Implementacion de la ruta getByName

--- backend/routes/RolRoutes.js
+++ backend/routes/RolRoutes.js
@@ -7,7 +7,7 @@ const verificationRol = require("../middlewares/VerificationRol");
 
 router.get("/getAll", verificationToken, verificationRol(["superAdmin"]), rolController.findAll);
 
-
+router.get("/getByName/:name", verificationToken, rolController.getByName);
 
 router.post("/create", rolController.create);
 
