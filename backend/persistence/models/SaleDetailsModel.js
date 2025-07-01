25a412be4d0cd05b148bcdfaef99c97cabae18a2
Daniel-iwach <daniel.g.iwach@gmail.com>
2025-06-30 22:55:11 -0300

    actualizacion de modelado para productId y saleId cambiarlos de String a objectId

--- backend/persistence/models/SaleDetailsModel.js
+++ backend/persistence/models/SaleDetailsModel.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 
 const saleDetailsSchema = new mongoose.Schema({
-  saleId: { type:String, required: true },
-  productId: { type: String, required: true },
+  saleId: { type: mongoose.Schema.Types.ObjectId, ref: "Sale", required: true },
+  productId: { type: mongoose.Schema.Types.ObjectId, ref: "Product", required: true },
   quantity: { type: Number, required: true },
   subtotal: { type: Number, required: true }
 });
