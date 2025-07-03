700bcb80ce304cc00b115f46c3bc3ebc8acb530d
Daniel-iwach <daniel.g.iwach@gmail.com>
2025-07-03 18:50:44 -0300

    actualizacion de metodo getSalesDetailsBySaleId para traer el producto embebido

--- backend/persistence/repositories/SaleDetailsRepository.js
+++ backend/persistence/repositories/SaleDetailsRepository.js
@@ -5,7 +5,7 @@ const getById = id => SaleDetails.findById(id);
 const create = data => SaleDetails.create(data);
 const update = (id, data) => SaleDetails.findByIdAndUpdate(id, data, { new: true });
 const remove = id => SaleDetails.findByIdAndDelete(id);
-const getBySaleId = (saleId) => SaleDetails.find({ saleId });
+const getBySaleId = (saleId) => SaleDetails.find({ saleId }).populate('productId');
 const getTopProducts = () => SaleDetails.aggregate([
                             {
                                 $group: {
