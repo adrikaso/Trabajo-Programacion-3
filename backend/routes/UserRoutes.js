1bb4e54be7c77fbbf1bc9d2f95aaf948bf81425f
Daniel-iwach <daniel.g.iwach@gmail.com>
2025-07-02 23:48:09 -0300

    implementacion de ruta para actualizar ususario

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -6,7 +6,9 @@ const verificationToken = require('../middlewares/VerificationToken');
 const verificationRol = require('../middlewares/VerificationRol');
 
 router.get('/getAll', userController.getAllUsers);
+router.get('/getById/:id', userController.getUserById);
 router.get('/getUser/:email', userController.getUserByEmail);
+router.put('/update/:id', userController.updateUser);
 router.post('/create', verificationToken, verificationRol(['superAdmin']), userController.createUser);
 
 module.exports = router;
