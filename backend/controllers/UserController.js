0d400102738dbc72bf405713af92976a6a0a3316
Daniel-iwach <daniel.g.iwach@gmail.com>
2025-07-02 23:47:17 -0300

    implementacion de metodo updateUser

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,4 +1,5 @@
 const services = require("../services/UserService");
+const bcrypt = require("bcryptjs");
 
 function createUser(req, res) {
     const data = req.body;
@@ -26,4 +27,24 @@ function getUserByEmail(req, res) {
     });
 }
 
-module.exports = {createUser, getAllUsers, getUserByEmail};
+function getUserById(req, res) {
+    const id = req.params.id;
+    services.getUserById(id).then((result) => {
+        res.status(201).json(result);
+    }).catch((err) => {
+        res.status(500).json({ message: err.message });
+    });
+}
+
+function updateUser(req, res) {
+    const id = req.params.id;
+    const data = req.body;
+    data.password = bcrypt.hashSync(data.password, 10);
+    services.updateUserById(id, data).then((result) => {
+        res.status(201).json(result);
+    }).catch((err) => {
+        res.status(500).json({ message: err.message });
+    });
+}
+
+module.exports = {createUser, getAllUsers, getUserByEmail, getUserById, updateUser};
