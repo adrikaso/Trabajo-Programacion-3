cc1cd80bfb4f578abc476f7d42a8628fd5107a98
Daniel-iwach <daniel.g.iwach@gmail.com>
2025-07-04 11:29:11 -0300

    implementacion para redirigir con combinacion de teclas al login de admin

--- frontend/assets/js/index.js
+++ frontend/assets/js/index.js
@@ -1,4 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
+
+    document.addEventListener("keydown", function(event) {
+        if (event.ctrlKey && event.shiftKey && event.key === "A") {
+            window.location.href = 'login.html';
+        }
+    });
+
     const btnCreateCart = document.getElementById('btnCrearCarrito');
     const inputClientName = document.getElementById('inputClientName');
 
