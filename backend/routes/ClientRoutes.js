edb9833278d9c28c2b566ca44502d78acbf1a713
Adrian <adr1337@outlook.com>
2025-07-07 20:45:51 -0300

    Eliminación de cliente

--- backend/routes/ClientRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const clientController = require("../controllers/ClientController");
-
-router.get('/getAll', clientController.getAllClients);
-router.post('/create', clientController.createClient);
-
-module.exports = router;
